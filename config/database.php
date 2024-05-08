e0200e9b92aab19cb8a89cc2b65499dd4b128a42
Veeru7 <101180463+Veeru7@users.noreply.github.com>
2024-05-08 19:44:45 +0100

    fixed the user login

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 define('DB_HOST',      'localhost');
 define('DB_PATH',      '');
 define('DB_CHARSET',   'utf8mb4');
-define('DB_UNAME',     'root'); // $_ENV['DB_UNAME']
+define('DB_UNAME',     'veeru'); // $_ENV['DB_UNAME']
 define('DB_PWORD',     'Cst@1985'); // $_ENV['DB_PWORD']
 define('DB_NAME',  'resolve'); // DB_NAME[0]
 
